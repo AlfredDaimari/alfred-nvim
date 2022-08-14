88bcf60fd697a207050bc901d720ab75af9b5b0d
Alfred Daimari <alfreddaimari77@gmail.com>
2022-08-14 19:58:31 +0530

    removed utils

--- lua/plugins/fzflua.lua
+++ lua/plugins/fzflua.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_set_keymap('n', '<c-P>',
-    "<cmd>lua require('fzf-lua').files()<CR>",
-    { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+        'n', '<c-P>',
+        "<cmd>lua require('fzf-lua').files()<CR>",
+        { noremap = true, silent = true }
+)
 
